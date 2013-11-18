85eb96587553a7aae3e56775c6508a899442c97b
mcgj <hn_mcgj@163.com>
2013-11-18 01:17:44 -0800

    Add m-from-1.0.js doc

--- m-js/src/m-from-1.0.js
+++ m-js/src/m-from-1.0.js
@@ -1,8 +1,29 @@
 function Mjf(){};
 Mjf.prototype = new Object();
+
+/**
+ * Ajax Post
+ * @param {String} type
+ * @param {String} url
+ * @param {String} data
+ * @param {String} async
+ * @param {String} dataType
+ * @param {array} names
+ * @param {array} values
+ * @returns {String}
+ */
 Mjf.Post = function(type,url,data,async,dataType,names,values){
-	
+	var result = "";
+	return result;
 };
+
+/**
+ * 构建请求参数
+ * @param {array} names
+ * @param {array} values
+ * @returns {String}
+ */
 Mjf.BuildParam = function(names,values){
-	var param = "";
+	var params = "";
+	return params;
 };
